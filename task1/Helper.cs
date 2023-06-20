90d9b6eea42dfca4cf7352000265090126a7f77d
Настя <nastamezenceva9155@gmail.com>
2023-06-21 01:33:47 +0300

    Добавьте файлы проекта.

---
+++ task1/Helper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Helper
+    {
+        public static int[][] String_to_array(string[] str)
+        {
+            int[][] _new = new int[Convert.ToInt32(str[0])][];
+            int number_of_products = Convert.ToInt32(str[1].ToString());
+            int k = 0;
+            for (int i = 1; i < str.Length; i++)
+            {
+                int l = 0;
+                //Узнаем количество продуктов, т.к. они находятся только на нечетных строчках
+                if (i % 2 != 0)
+                {
+                    number_of_products = Convert.ToInt32(str[i].ToString());
+                    _new[k] = new int[number_of_products];
+                    i++;
+                }
+
+                for (int j = 0; j < number_of_products; j++)
+                {
+                    string[] new_str = GetStrings(str[i], number_of_products);
+                    _new[k][l] = Convert.ToInt32(new_str[j]);
+                    l++;
+                }
+                k++;
+            }
+            return _new;
+        }
+        static string[] GetStrings(string str, int num)
+        {
+            string[] strings = new string[num]; int i = 0, k = 0; string number = "";
+
+            while (i < str.Length)
+            {
+                if (str[i] != ' ')
+                {
+                    number += str[i];
+                }
+                else
+                {
+                    strings[k] = number;
+                    k++;
+                    number = "";
+                }
+                i++;
+            }
+            if (!number.Equals(""))
+            {
+                if (strings[k] == null)
+                {
+                    strings[k] = number;
+                }
+                else
+                {
+                    throw new Exception("массив полон, но осталось лишнее число");
+                }
+                number = "";
+            }
+
+            return strings;
+        }
+    }
+}
---
+++ task1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Immutable;
+using System.IO;
+
+namespace task1
+{
+    internal class Program
+    {
+        static string Path = @"..\..\..\..\test.txt";
+        static int[][] Test;
+        static int[] result;
+
+        static void ReadFile(string path)
+        {
+            string[] file = File.ReadAllLines(path);
+            Test = Helper.String_to_array(file);
+            result = new int[Convert.ToInt32(file[0].ToString())];
+        }
+
+        static void Processing()
+        {
+            int lenght = Test.Length;
+            
+            for (int i = 0; i < lenght; i++) 
+            {
+                int[] arr = Test[i];
+                Array.Sort(arr);
+                int quantity = 1, sum = 0;
+
+                if(arr.Length == 1) 
+                {
+                    sum += arr[0];
+                }
+                else 
+                {
+                    for (int j = 1; j < arr.Length; j++)
+                    {
+                        if (arr[j] != arr[j - 1] || j == arr.Length - 1)
+                        {
+                            if (j == arr.Length - 1)
+                            {
+                                quantity++;
+                            }
+                            int whole_part = quantity / 3 * 2;
+                            int remains = quantity % 3;
+                            sum += (whole_part + remains) * arr[j - 1];
+                            quantity = 1;
+                        }
+                        else
+                        {
+                            quantity++;
+                        }
+
+                    }
+                }
+                result[i] = sum;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ReadFile(@"..\..\..\..\test.txt");
+            Processing();
+
+            for(int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+
+       
+    }
+
+    
+}
